a7238fa80b031a61239ead6a72a6035c0883d93f
Maxime Welsh <1333794@cegepoutaouais.qc.ca>
2020-12-18 12:54:18 -0500

    modification class client

--- projetfinal/Client.cs
+++ projetfinal/Client.cs
@@ -6,24 +6,63 @@ using System.Threading.Tasks;
 
 namespace projetfinal
 {
-    public static class InfoCompte
+    public class Client : Perssone
     {
-        private static Dictionary<int, Client> listClients;
 
-        public static Dictionary<int, Client> ListeClients
+        //Champs privés
+        private int num_client;
+        private string nom_compte;
+        private string mots_passe;
+
+        //Accesseurs (propriétés)
+        public int NumeroClient
         {
-            get { return listClients; }
-            set { listClients = value; }
+            get { return num_client; }
+            set { num_client = value; }
+        }
+        public string NomCompte
+        {
+            get { return nom_compte; }
+            set { nom_compte = value; }
+        }
+        public string MotsPasse
+        {
+            get { return mots_passe; }
+            set { mots_passe = value; }
         }
 
-        static InfoCompte()
+        public Client()
         {
-            listClients = new Dictionary<int, Client>();
+            num_client = 0;
+            nom_compte = "";
+            mots_passe = "";
+
         }
 
-        public static void CompterNb()  // compte le nb de client dans la liste ??
+        public Client(string n, string p, char g, DateTime dt, string adr, string tel, string c, int numclient, string nmcompt, string mtspss) : base(n, p, g, dt, adr, tel, c)
         {
-            listClients.Count();
+            num_client = numclient;
+            nom_compte = nmcompt;
+            mots_passe = mtspss;
+
+        }
+
+
+        // Méthode surchargée dans la classe fille client
+        public override string[] ToArray()
+        {
+            //récuperer le tableau des proprietés de la classe parent personne
+            string[] proprietes1 = base.ToArray();
+            // créer un tableau pour acceuillir toutes les proprietés
+            string[] proprietes = new string[proprietes1.Length + 2];
+            // copier les proprietés héritées  
+            proprietes1.CopyTo(proprietes, 0);
+            // compléter le tableau proprietes avec les propriétés de la classe fille
+            proprietes[7] = num_client.ToString();
+            proprietes[8] = nom_compte;
+            proprietes[9] = mots_passe;
+            return proprietes;
+
         }
 
     }
